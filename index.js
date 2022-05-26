e133642c118aed5a9714c21e3c4816b5ddce047f
Milen Todev <todevcode@gmail.com>
2022-05-26 21:35:15 +0300

    add: gravity to the player and enemy

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2;
+
 class Sprite {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
+    this.height = 150
   }
   draw() {
     c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, 50, 150);
+    c.fillRect(this.position.x, this.position.y, 50, this.height);
+  }
+  update() {
+      this.draw()
+      this.position.y += this.velocity.y
+
+      if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+          this.velocity.y = 0
+    } else this.velocity.y += gravity
   }
 }
+
 
 const player = new Sprite({
   position: {
@@ -27,8 +39,6 @@ const player = new Sprite({
     y: 0,
   },
 });
-
-player.draw();
 
 const enemy = new Sprite({
     position: {
@@ -45,6 +55,10 @@ enemy.draw();
 
 function animate() {
   window.requestAnimationFrame(animate);
+  c.fillStyle = 'black'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  player.update();
+  enemy.update();
 }
 
 animate();
